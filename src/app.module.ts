7e172f16460b51fd045f9af25421d0315a91c6ae
AbuMusa <amsohag007@gmail.com>
2022-06-12 17:41:17 +0600

    mongodb crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('DB_URI')],
   controllers: [],
   providers: [],
 })
